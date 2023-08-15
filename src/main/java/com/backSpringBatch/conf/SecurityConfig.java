77644b6bef73cc66a34e3c39b2fd110bcfbf7424
EduContreras09 <mcontreras@procamaronex.com>
2023-08-15 11:45:03 -0500

    problema de aseguramiento con MS #CB-682

--- src/main/java/com/backSpringBatch/conf/SecurityConfig.java
+++ src/main/java/com/backSpringBatch/conf/SecurityConfig.java
@@ -1,36 +1,20 @@
 package com.backSpringBatch.conf;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig implements WebMvcConfigurer {
-
-    private final ExternalServersConfig externalServersConfig;
-
-    @Autowired
-    public SecurityConfig(ExternalServersConfig externalServersConfig) {
-        this.externalServersConfig = externalServersConfig;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/","/**").permitAll().anyRequest().
-                hasRole("ADMIN").and().csrf().disable();
-        return http.build();
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").
-                allowedOrigins(externalServersConfig.getCors()).
-                allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
